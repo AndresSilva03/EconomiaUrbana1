2583cd006c3feb8d38d23bb27f893a8d32317746
AndresSilva03 <af.silvag1@uniandes.edu.co>
2024-08-11 11:47:05 -0500

    Regresiones

---
+++ Taller 1/Taller1.R
@@ -0,0 +1,53 @@
+
+## Andrés Silva
+## Juan Andrés Roldan
+## Camilo Carrascal
+
+rm(list=ls())
+
+require(pacman)
+p_load(tidyverse, rio, 
+       arrow, ## read parque files
+       broom, # tidy-coefficients
+       mfx, # marginal effects
+       margins,  # marginal effects
+       estimatr, # robust standard errors
+       lmtest, # HAC (Newey-West) standard errors
+       fixest, # hdfe regressions (feols)
+       modelsummary, # Coefplot with modelplot
+       stargazer, # export tables to latex 
+       xtable #exportar a latex
+) 
+
+setwd(dir = "C:/Users/silva/OneDrive/Escritorio/2024-II
+      /Economia Urbana/EconomiaUrbana1") # fijamos el directorio
+
+df <- import("Taller 1/dataTaller1.rds") # llamamos a la base de datos
+
+# ?lm
+
+lconsumo <- log(df$consumo)
+lingreso <- log(df$ingreso)
+
+df <- mutate(.data = df, lconsumo = log(df$consumo))
+df <- mutate(.data = df, lingreso = log(df$ingreso))
+
+regresion1 <- lm(formula = df$lconsumo ~ df$voucher)
+regresion1 %>% summary()
+
+regresion2 <- lm(formula = df$lconsumo ~ df$voucher + df$lingreso)
+regresion2 %>% summary()
+
+
+stargazer(regresion1, regresion2,
+          type= "latex",
+          dep.var.labels = c('',"lconsumo",''), 
+          df = FALSE,
+          digits = 4, 
+          out = paste0("Output/ols.tex"),
+          title = "Tabla 1. Modelos de regresión",
+          style = "default"
+          )
+
+
+#?stargazer
